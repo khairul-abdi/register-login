f44c1873d969c159ff14c1f6a4c3234661d74caa
Linuxer <linuxer@abdi.local>
2020-03-11 16:14:18 +0700

    make it better

--- core/pool.js
+++ core/pool.js
@@ -4,8 +4,8 @@ const mysql = require('mysql')
 // Connection to the database
 const pool = mysql.createPool({
   connectionLimit: 10,
-  hostname: 'us-cdbr-iron-east-04.cleardb.net',
-  username: 'b718451d97f87a',
+  host: 'us-cdbr-iron-east-04.cleardb.net',
+  user: 'b718451d97f87a',
   password: '54fb84e5',
   database: 'heroku_d94675a72978852'
 })
